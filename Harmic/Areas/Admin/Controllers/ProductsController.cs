78cc03b0c8bf9760ca8ee74138aaf94fde898faf
quanzz123 <nguyenquandc37@gmail.com>
2024-11-11 10:46:29 +0700

    edit produccôntroller

--- Harmic/Areas/Admin/Controllers/ProductsController.cs
+++ Harmic/Areas/Admin/Controllers/ProductsController.cs
@@ -48,7 +48,7 @@ namespace Harmic.Areas.Admin.Controllers
         // GET: Admin/Products/Create
         public IActionResult Create()
         {
-            ViewData["CategoryProductId"] = new SelectList(_context.TbProductCategories, "CategoryProductId", "CategoryProductId");
+            ViewData["CategoryProductId"] = new SelectList(_context.TbProductCategories, "CategoryProductId", "Title");
             return View();
         }
 
