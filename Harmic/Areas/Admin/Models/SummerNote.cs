b7a62843f8562ed68704452deb29714302265226
quanzz123 <nguyenquandc37@gmail.com>
2024-11-16 14:53:22 +0700

    file manager and upload image

---
+++ Harmic/Areas/Admin/Models/SummerNote.cs
@@ -0,0 +1,27 @@
+﻿namespace Harmic.Areas.Admin.Models
+{
+    public class SummerNote
+    {
+        public SummerNote(string idEditor, bool loadLibary = true) {
+
+            IDEditor = idEditor;
+            LoadLibary = loadLibary;
+
+        }
+        public string IDEditor { get; set; }
+        public bool LoadLibary { get; set; }
+        public int Height { get; set; } = 500;
+        public string toolbar { get; set; } = @"
+            [
+                ['style', ['style']],
+                ['font', ['bold', 'underline', 'clear']],
+                ['color', ['color']],
+                ['para', ['ol', 'ul', 'paragraph']],
+                ['table',['table']],
+                ['insert', ['link', 'elfinderFile', 'video', 'elfinder']],
+                ['view', ['fullscreen', 'codeview', 'help']],
+                
+            ]
+        ";
+    }
+}
